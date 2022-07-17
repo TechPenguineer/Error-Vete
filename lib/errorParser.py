b635acb63ece392d2e586af9a032e855f1bfb846
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2022-07-17 14:06:16 -0400

    main parser

--- lib/errorParser.py
+++ lib/errorParser.py
@@ -1,12 +1,22 @@
-#  File "d:\Desktop\ErrorVete\Error-Vete\__test.py", line 2, in <module>
-#    print("Total number of days: " + days)
-# TypeError: can only concatenate str (not "int") to str
+ERROR_MSG = """  File "d:\Desktop\ErrorVete\Error-Vete\__test.py", line 2, in <module>
+    print("Total number of days: " + days)
+ TypeError: can only concatenate str (not "int") to str"""
 
 
 class EVParser:
     def __init__(self):
         self.fileLocation
+        self.errorLine
         self.statement
         self.typeError
+
+    def removeCommaSep(data):
         
-    def parseErrorMessage(msg):
+    def parseErrorMessage(msg, self):
+        data = msg.split()
+        self.errorLine = data[3]
+        self.fileLocation = data[1]
+        print("Error @:\n\nLine: " + self.errorLine + "\nOf File: " + self.fileLocation)
+
+
+EVParser.parseErrorMessage(ERROR_MSG, EVParser)
